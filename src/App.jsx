214d47fb8ce4a85d5d5d17191b4484f3a4dc6035
najeebmosab <73877639+najeebmosab@users.noreply.github.com>
2023-02-16 11:13:02 +0200

    Add EX5.1 react

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { Quiz } from "./EX4/EX4.2"
+import {ButtonComponents} from "./Buttons/ButtonComponents"
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Quiz></Quiz>
+      < ButtonComponents text={"important"} bold={"bold"}/>
+      < ButtonComponents text={"No't important"} blod={""}/>
     </>
   )
 }
---
+++ src/Buttons/ButtonComponents.jsx
@@ -0,0 +1,8 @@
+function ButtonComponents(props) {
+    return (
+    <>
+        <button style={{fontWeight:props.bold}}>{props.text}</button>
+    </>
+    )
+}
+export {ButtonComponents};
